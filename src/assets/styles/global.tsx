e27e5f44897b8b4d0f319453e6213930ca9e4f3b
Yuri Cianci <yucianci@gmail.com>
2022-12-16 21:28:02 -0300

    :heavy_plus_sign: Feat: Add styled-components and created global style file

---
+++ src/assets/styles/global.tsx
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  html, body, #root {
+    font-family: 'Poppins', sans-serif;
+    color-scheme: light dark;
+    color: #ecf0f1;
+    background-color: #242424;
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+    width: 100%;
+    height: 100%;
+  }
+  *:focus {
+    outline: 0;
+  }
+  a {
+    text-decoration: none;
+  }
+  ul {
+    list-style: none;
+  }
+  button {
+    cursor: pointer;
+  }
+`;
